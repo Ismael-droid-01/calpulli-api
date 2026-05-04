ac808f0d9b00fb0340d8c0a783560e9e596adc03
Ignacio Castillo <ignacio.bcastillo@gmail.com>
2026-05-04 09:52:05 -0600

    fix the create_scope script

--- create_scope.sh
+++ create_scope.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 readonly scope=${1:-"calpulli"}
-
+readonly ACCOUNT_ID="calpulli_ci"
 curl --request POST \
-  --url http://localhost:10000/api/v4/scopes \
+  --url http://localhost:10000/api/v4/accounts/$ACCOUNT_ID/scopes \
   --header 'Content-Type: application/json' \
   --data '{
   "name":"'"$scope"'"
